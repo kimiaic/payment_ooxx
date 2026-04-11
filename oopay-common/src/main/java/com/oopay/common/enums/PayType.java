89b3c8cbcca51472745e2aefc7ed55494991d307
kimiaic <kimiai.claw@proton.me>
2026-04-11 15:04:20 +0800

    feat(phase1): add base entity and enums

--- oopay-common/src/main/java/com/oopay/common/enums/PayType.java
+++ oopay-common/src/main/java/com/oopay/common/enums/PayType.java
@@ -3,68 +3,26 @@ package com.oopay.common.enums;
 import lombok.Getter;
 
 /**
- * 支付类型枚举
+ * 支付方式枚举
  */
 @Getter
 public enum PayType {
 
-    /**
-     * 支付宝
-     */
-    ALIPAY(1, "支付宝"),
+    WECHAT_NATIVE("WECHAT_NATIVE", "微信支付-扫码"),
+    WECHAT_JSAPI("WECHAT_JSAPI", "微信支付-公众号"),
+    ALIPAY_QR("ALIPAY_QR", "支付宝-扫码"),
+    ALIPAY_WAP("ALIPAY_WAP", "支付宝-手机"),
+    USDT_TRC20("USDT_TRC20", "USDT-TRC20");
 
-    /**
-     * 微信支付
-     */
-    WECHAT(2, "微信支付"),
-
-    /**
-     * 银联
-     */
-    UNIONPAY(3, "银联"),
-
-    /**
-     * 数字货币
-     */
-    CRYPTO(4, "数字货币"),
-
-    /**
-     * 话费充值
-     */
-    PHONE(5, "话费充值"),
-
-    /**
-     * 电费充值
-     */
-    ELECTRIC(6, "电费充值"),
-
-    /**
-     * 油卡充值
-     */
-    OIL(7, "油卡充值"),
-
-    /**
-     * 信用卡代付
-     */
-    CREDIT_CARD(8, "信用卡代付"),
-
-    /**
-     * 其他
-     */
-    OTHER(99, "其他");
-
-    private final Integer code;
+    private final String code;
     private final String desc;
 
-    PayType(Integer code, String desc) {
+    PayType(String code, String desc) {
         this.code = code;
         this.desc = desc;
     }
 
-    public static PayType of(Integer code) {
-        if (code == null) {
-            return null;
-        }
+    public static PayType fromCode(String code) {
         for (PayType type : values()) {
             if (type.code.equals(code)) {
                 return type;
