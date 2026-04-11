e7f30dc13a9ac6f0973463bc44867c45d21d5ac4
kimiaic <kimiai.claw@proton.me>
2026-04-11 20:45:18 +0800

    feat(phase1): add base entity and enums

--- oopay-common/src/main/java/com/oopay/common/enums/PayType.java
+++ oopay-common/src/main/java/com/oopay/common/enums/PayType.java
@@ -2,17 +2,11 @@ package com.oopay.common.enums;
 
 import lombok.Getter;
 
-/**
- * 支付方式枚举
- */
 @Getter
 public enum PayType {
-
-    WECHAT_NATIVE("WECHAT_NATIVE", "微信支付-扫码"),
-    WECHAT_JSAPI("WECHAT_JSAPI", "微信支付-公众号"),
-    ALIPAY_QR("ALIPAY_QR", "支付宝-扫码"),
-    ALIPAY_WAP("ALIPAY_WAP", "支付宝-手机"),
-    USDT_TRC20("USDT_TRC20", "USDT-TRC20");
+    WECHAT("WECHAT", "微信支付"),
+    ALIPAY("ALIPAY", "支付宝"),
+    USDT("USDT", "USDT");
 
     private final String code;
     private final String desc;
@@ -20,14 +14,5 @@ public enum PayType {
     PayType(String code, String desc) {
         this.code = code;
         this.desc = desc;
-    }
-
-    public static PayType fromCode(String code) {
-        for (PayType type : values()) {
-            if (type.code.equals(code)) {
-                return type;
-            }
-        }
-        return null;
     }
 }
