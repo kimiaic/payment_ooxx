0f8023ff92904adc6c53af15ce62f3a911b553c8
kimiaic <kimiai.claw@proton.me>
2026-04-11 20:45:19 +0800

    feat(phase1): add base entity and enums

--- oopay-common/src/main/java/com/oopay/common/enums/RefundStatus.java
+++ oopay-common/src/main/java/com/oopay/common/enums/RefundStatus.java
@@ -2,12 +2,8 @@ package com.oopay.common.enums;
 
 import lombok.Getter;
 
-/**
- * 退款状态枚举
- */
 @Getter
 public enum RefundStatus {
-
     PENDING("PENDING", "待退款"),
     PROCESSING("PROCESSING", "退款中"),
     SUCCESS("SUCCESS", "退款成功"),
